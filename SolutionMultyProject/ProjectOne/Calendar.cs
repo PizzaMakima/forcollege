2e5750a878e4d46d6a7b414609eb62c977e4d66b
pizzamakima <cyrillpuzin@gmail.com>
2024-11-25 04:26:47 +0300

    update

---
+++ SolutionMultyProject/ProjectOne/Calendar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProjectOne
+{
+    public class Calendar
+    {
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            MessageBox.Show($"Today's date is {now.ToShortDateString()}", "Current Date");
+        }
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
